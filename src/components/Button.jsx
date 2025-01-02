76a14d0cbba95aca838f8fd48518a25f3b4bb1f5
Christopher Benedicto <benedicto.christopheeer@gmail.com>
2025-01-03 05:05:19 +0800

    Hero Video - Animation with gsap - Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div> {title}</div>
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  leftIcon: PropTypes.node,
+  rightIcon: PropTypes.node,
+  containerClass: PropTypes.string,
+  onClick: PropTypes.func,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+export default Button;
